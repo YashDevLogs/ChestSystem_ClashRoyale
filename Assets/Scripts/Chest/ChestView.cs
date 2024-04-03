0355462ccb89cd1b4be2e7d6f186370e3a0291e7
Yash <91522919+YashDevLogs@users.noreply.github.com>
2024-04-03 13:44:32 +0530

    Created UI panels
    Created UI panels for popups etc

--- Assets/Scripts/Chest/ChestView.cs
+++ Assets/Scripts/Chest/ChestView.cs
@@ -1,12 +1,27 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Chest
 {
     public class ChestView : MonoBehaviour
     {
+        public ChestController Controller;
+        public Slot SlotReference;
+
+        [SerializeField] private Sprite emptySlotSprite;
+        [SerializeField] public Text ChestTimerText;
+        [SerializeField] private Text chestSlotSprite;
+        [SerializeField] private Text chestTypeText;
+        [SerializeField] private Image coinSpirte;
+        [SerializeField] private Text coinText;
+        [SerializeField] private Image gemSpirte;
+        [SerializeField] private Text gemText;
+
+        [SerializeField] private Button chestButton;
 
 
-        
+
+
     }
 }
---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Chest;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Slot : MonoBehaviour
+    {
+
+        [SerializeField] ChestView chestView;
+        public bool isEmpty;
+        [HideInInspector]
+        public ChestController chestController;
+
+
+        private void Start()
+        {
+            isEmpty = true;
+        }
+
+
+    }
+}
